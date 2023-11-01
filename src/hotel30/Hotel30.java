b991d581fe799a4ef53b2deca9c7f5e31c7576fa
Noxximass <121057640+Noxximass@users.noreply.github.com>
2023-11-01 01:58:54 -0300

    .

--- src/hotel30/Hotel30.java
+++ src/hotel30/Hotel30.java
@@ -1,6 +1,11 @@
 
 package hotel30;
 
+import Datos.UsuariosData;
+import Entidades.Usuarios;
+import java.time.LocalDate;
+import java.time.Month;
+
 
 
 public class Hotel30 {
@@ -9,8 +14,10 @@ public class Hotel30 {
 
   public static void main(String[] args) { 
       
-
-        
+//Usuarios us=new Usuarios("tmvannucci", "tomas123",40823370, "Tomas", "Moyano Vannucci",LocalDate.of(1997, Month.NOVEMBER, 26), "Jose Marti 116", "Administrativo");
+UsuariosData usd= new UsuariosData();
+//usd.agregarEmpleado(us);
+usd.ingresoDeEmpleado("tmvannucci", "tomas123");
     }
     
 }
